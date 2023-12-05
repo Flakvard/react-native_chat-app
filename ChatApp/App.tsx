f2249354bfbab23b2617d8ca755a562a9af69667
flakvard <marni@falkvard.com>
2023-12-05 01:08:09 +0100

    simple login componant UI test

--- ChatApp/App.tsx
+++ ChatApp/App.tsx
@@ -9,52 +9,42 @@ import React from 'react';
 import { ScrollView, StatusBar, StyleSheet} from 'react-native';
 
 import {Block, Button, Text, Input} from './src/common/components'
+import { Login } from './src/features/auth/components';
 
 function App() {
+  return <Login />
 
-  return (
-    <ScrollView>
-        <Block flex={3} color="#fff" align="center" justify="center">
-            <StatusBar barStyle={"dark-content"}/>
-            <Text h1>Header 1</Text>
-            <Text h2>Header 2</Text>
-            <Text h3>Header 3</Text>
-            <Text h4>Header 4</Text>
-            <Text p>Paragraph</Text>
-            <Text>Text</Text>
-              <Block row>
-                <Text>Hello world</Text>
-              </Block>
-              <Block style={{marginTop: 12, }}>
-                <Text h3>Button</Text>
-                <Button>
-                  <Text>button</Text>
-                </Button>
-                <Button color="purple" radius={16} paddingVertical={6} paddingHorizontal={20}>
-                  <Text align="center" color="white">button</Text>
-                </Button>
-              </Block>
-              <Block style={{marginTop: 12, }}>
-                <Text h3>Inputs</Text>
-                <Input keyboardType='email-address' placeholder='Email' placeholderTextColor="black"/>
-                <Input secureTextEntry placeholder='Password' placeholderTextColor="black"/>
-              </Block>
-        </Block>
-    </ScrollView>
-  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 2,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingHorizontal: 24,
-    fontSize: 24,
-    fontWeight: '600',
-    textAlign: "center",
-  },
-});
-
 export default App;
+
+  // return (
+    // <ScrollView>
+        // <Block flex={3} color="#fff" align="center" justify="center">
+            // <StatusBar barStyle={"dark-content"}/>
+            // <Text h1>Header 1</Text>
+            // <Text h2>Header 2</Text>
+            // <Text h3>Header 3</Text>
+            // <Text h4>Header 4</Text>
+            // <Text p>Paragraph</Text>
+            // <Text>Text</Text>
+              // <Block row>
+                // <Text>Hello world</Text>
+              // </Block>
+              // <Block style={{marginTop: 12, }}>
+                // <Text h3>Button</Text>
+                // <Button>
+                  // <Text>button</Text>
+                // </Button>
+                // <Button color="purple" radius={16} paddingVertical={6} paddingHorizontal={20}>
+                  // <Text align="center" color="white">button</Text>
+                // </Button>
+              // </Block>
+              // <Block style={{marginTop: 12, }}>
+                // <Text h3>Inputs</Text>
+                // <Input keyboardType='email-address' placeholder='Email' placeholderTextColor="black"/>
+                // <Input secureTextEntry placeholder='Password' placeholderTextColor="black"/>
+              // </Block>
+        // </Block>
+    // </ScrollView>
+  // );
+// }
---
+++ ChatApp/src/features/auth/components/Login.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Block, Button, Input, Text} from '../../../common/components';
+
+
+function Login() {
+  return (
+    <Block marginTop={50} paddingButtom={20}>
+      <Text h3 align="center">Login</Text>
+        <Input keyboardType='email-address' placeholder='Email' placeholderTextColor="black"/>
+        <Input secureTextEntry placeholder='Password' placeholderTextColor="black"/>
+        <Button color="lightblue" radius={8} padding={16}>
+            <Text align="center">
+                Login
+            </Text>
+        </Button>
+    </Block>
+  )
+}
+export default Login;
