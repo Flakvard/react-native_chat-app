c0663efa735a5d20e5bfbe25a43e711dc154e6e4
flakvard <marni@falkvard.com>
2023-12-12 12:12:18 +0100

    componants for the Chat feature

---
+++ ChatApp/src/features/chat/components/ChatRoom.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import useTheme, { ThemeProvider } from '../../../common/hooks/useTheme';
+import { Block, Button, Text } from '../../../common/components'
+
+const ChatRoom = ({navigation}) => {
+  const {sizes, colors} = useTheme();
+  return (
+    <Block flex={1} color={colors.secondary}>
+      <Text>ChatRoom</Text>
+          <Button marginTop={sizes.m} onPress={() => navigation.navigate('ChatRoom')}>
+              <Text p weight='bold' align='right'>Continue to ChatRoom ? Click here</Text> 
+          </Button>
+          <Button marginTop={sizes.m} onPress={() => navigation.navigate('RoomList')}>
+              <Text p weight='bold' align='right'>Continue to RoomList ? Click here</Text> 
+          </Button>
+          <Button marginTop={sizes.m} onPress={() => navigation.navigate('Message')}>
+              <Text p weight='bold' align='right'>Continue to Message ? Click here</Text> 
+          </Button>
+    </Block>
+  )
+}
+
+export default ChatRoom
---
+++ ChatApp/src/features/chat/components/RoomList.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import useTheme, { ThemeProvider } from '../../../common/hooks/useTheme';
+import { Block, Button, Text } from '../../../common/components'
+
+const RoomList = ({navigation}) => {
+  const {sizes, colors} = useTheme();
+  return (
+    <Block flex={1} color={colors.secondary}>
+      <Text>RoomList</Text>
+          <Button marginTop={sizes.m} onPress={() => navigation.navigate('ChatRoom')}>
+              <Text p weight='bold' align='right'>Continue to ChatRoom ? Click here</Text> 
+          </Button>
+          <Button marginTop={sizes.m} onPress={() => navigation.navigate('RoomList')}>
+              <Text p weight='bold' align='right'>Continue to RoomList ? Click here</Text> 
+          </Button>
+          <Button marginTop={sizes.m} onPress={() => navigation.navigate('Message')}>
+              <Text p weight='bold' align='right'>Continue to Message ? Click here</Text> 
+          </Button>
+    </Block>
+  )
+}
+
+export default RoomList
