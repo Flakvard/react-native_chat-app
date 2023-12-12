98d0965316855bcd87fca5615af51e76e686c171
flakvard <marni@falkvard.com>
2023-12-12 02:07:30 +0100

    install react-native-stack

--- ChatApp/App.tsx
+++ ChatApp/App.tsx
@@ -1,32 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import React, { useEffect } from 'react';
-import { Platform, ScrollView, StatusBar, StyleSheet} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-
-import { Login } from './src/features/auth/components';
-import { ThemeProvider } from './src/common/hooks/useTheme';
-import SplashScreen from 'react-native-splash-screen';
+import React  from 'react';
+import PolyChat from './src/App';
 
 
-const App = () => {
-
-  useEffect(() => {
-    if (Platform.OS === "android")
-      SplashScreen.hide();
-  }, [])
-
-  return (
-    <NavigationContainer>
-      <ThemeProvider>
-        <Login />
-      </ThemeProvider>
-    </NavigationContainer>
-  );
-}
+const App = () => 
+  <PolyChat/>
 export default App;
