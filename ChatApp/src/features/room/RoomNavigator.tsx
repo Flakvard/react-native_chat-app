7d887e49da8cd3898f705b3a080ee3c4fc9115a8
flakvard <marni@falkvard.com>
2024-01-06 23:35:43 +0100

    RoomList and RoomListItem with dummy contacts via API

--- ChatApp/src/features/room/RoomNavigator.tsx
+++ ChatApp/src/features/room/RoomNavigator.tsx
@@ -1,12 +1,12 @@
 import { NativeStackScreenProps} from '@react-navigation/native-stack'
 import {RootParamList} from '../../navigation/AppNavigator'
-import { ChatRoom, Message } from './components';
+import { ChatRoom, Message } from '../chat/components';
 import { RoomList } from '../room/components';
 
 export type ChatRoomProps = NativeStackScreenProps<RootParamList, 'ChatRoom'>;
 
 export const ChatRoomScreen = ({ navigation, route }: ChatRoomProps) => {
-  return <ChatRoom navigation={navigation}/>;
+  return <ChatRoom navigation={navigation} route={undefined}/>;
 }
 
 export type MessageProps = NativeStackScreenProps<RootParamList, 'Message'>;
---
+++ ChatApp/src/features/room/components/RoomListItem.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableHighlight,
+  Text,
+  Image,
+  ViewStyle,
+  ImageStyle,
+  TextStyle
+} from "react-native";
+import {COLORS} from "../../../common/constants/theme";
+
+interface Props {
+  name: string;
+  avatar: string;
+  phone: string;
+  onPress(): void;
+}
+
+const RoomListItem: React.FC<Props> = ({ name, avatar, phone, onPress }) => {
+  return (
+    <TouchableHighlight
+      underlayColor={COLORS.grey}
+      style={styles.container}
+      onPress={onPress}
+    >
+      <View style={styles.contactInfo}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: avatar
+          }}
+        />
+
+        <View style={styles.details}>
+          <Text style={[styles.title]}>{name}</Text>
+          <Text style={styles.subtitle}>{phone}</Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+interface Style {
+  container: ViewStyle;
+  contactInfo: ViewStyle;
+  avatar: ImageStyle;
+  details: ViewStyle;
+  title: TextStyle;
+  subtitle: TextStyle;
+}
+
+const styles = StyleSheet.create<Style>({
+  container: {
+    paddingLeft: 24
+  },
+  contactInfo: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingRight: 24,
+    borderBottomColor: COLORS.grey,
+    borderBottomWidth: StyleSheet.hairlineWidth
+  },
+  avatar: {
+    borderRadius: 22,
+    width: 44,
+    height: 44
+  },
+  details: {
+    justifyContent: "center",
+    flex: 1,
+    marginLeft: 20
+  },
+  title: {
+    color: COLORS.black,
+    fontWeight: "bold",
+    fontSize: 16
+  },
+  subtitle: {
+    color: COLORS.blue,
+    fontSize: 15,
+    marginTop: 4
+  }
+});
+
+export default RoomListItem;
